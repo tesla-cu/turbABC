dc4e877f6d6d3194d6b860aa62c8fa861607397e
olgadoronina <olga.doronina@colorado.edu>
2020-07-23 11:24:47 -0600

    Remove overflow_result directory

---
+++ plotting/plot_overflow_chains.py
@@ -0,0 +1,35 @@
+import os
+import glob
+
+# import matplotlib as mpl
+# mpl.use('pdf')
+import numpy as np
+import plotting
+import plotting_2Dmarginals
+import plotting_change_with_eps
+
+
+def main():
+    basefolder = '../overflow_results/'
+
+    path = {'output': os.path.join(basefolder, 'chains_limits'), 'plots': os.path.join(basefolder, 'plots')}
+    if not os.path.isdir(path['plots']):
+        os.makedirs(path['plots'])
+    C_limits = np.loadtxt(os.path.join(path['output'], 'C_limits_init'))
+    nominal_values = [0.09, 0.075/0.09, 0.0828/0.09, 0.31]
+    params_names = [r'$\beta^*$', r'$\beta_1/\beta^*$', r'$\beta_2\beta^*$', r'$a_1$']
+    num_bin_kde = 100
+    folder = os.path.join(path['output'], 'postprocess/')
+    plot_folder = path['plots']
+    print("Plot comparison with true data and kde 2D marginals")
+    if not os.path.isdir(plot_folder):
+        os.makedirs(plot_folder)
+    c = np.loadtxt(os.path.join(folder, 'C_final_smooth{}'.format(num_bin_kde)))
+    plotting_2Dmarginals.plot_marginal_pdf(folder, C_limits, C_limits, num_bin_kde, params_names,
+                                           plot_folder, smooth="smooth")
+    ###################################################################################################################
+    print("Plot change of marginal pdfs for different epsilon")
+
+
+if __name__ == '__main__':
+    main()
